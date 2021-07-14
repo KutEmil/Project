4c97be463e38398fd6897c23987390cb8b2e7430
Emil <KutEmil@gmail.com>
2021-07-14 16:52:02 +0300

    add read and print methods

---
+++ src/main/java/test_project/model/Student.java
@@ -0,0 +1,67 @@
+package test_project.model;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Serializable;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.yaml.snakeyaml.Yaml;
+
+public class Student {
+
+	private String name;
+	private boolean sex;
+	private int age;
+	private int course;
+
+	public Student() {}
+
+	public Student(String name, boolean sex, int age, int course) {
+		this.name = name;
+		this.sex = sex;
+		this.age = age;
+		this.course = course;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean isSex() {
+		return sex;
+	}
+
+	public void setSex(boolean sex) {
+		this.sex = sex;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getCourse() {
+		return course;
+	}
+
+	public void setCourse(int course) {
+		this.course = course;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", sex=" + sex + ", age=" + age + ", course=" + course + "]";
+	}
+
+}
